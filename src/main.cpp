5af860cb5b18c1c2e0530410df3d8170209dc589
ceol22 <jordywk2004@gmail.com>
2019-05-02 15:17:13 +0200

    all functions written

--- src/main.cpp
+++ src/main.cpp
@@ -58,6 +58,129 @@ void digit1(int num1){
   }
 }
 
+void digit2(int num2){
+  switch(num2){
+    case 0:
+      leds[10] = CRGB::Black;
+    break;
+    case 1:
+       leds[7] = CRGB::Black;
+       leds[8] = CRGB::Black;
+       leds[10] = CRGB::Black;
+       leds[11] = CRGB::Black;
+       leds[12] = CRGB::Black;
+    break;
+    case 3:
+      leds[7] = CRGB::Black;
+      leds[11] = CRGB::Black;
+    break;
+    case 4:
+      leds[8] = CRGB::Black;
+      leds[11] = CRGB::Black;
+      leds[12] = CRGB::Black;
+    break;
+    case 5:
+      leds[9] = CRGB::Black;
+      leds[11] = CRGB::Black;
+    break;
+    case 6:
+      leds[9] = CRGB::Black;
+    break;
+    case 7:
+      leds[7] = CRGB::Black;
+      leds[10] = CRGB::Black;
+      leds[11] = CRGB::Black;
+      leds[12] = CRGB::Black;
+    break;
+    case 9:
+      leds[11] = CRGB::Black;
+    break;
+
+  }
+}
+
+void digit3(int num3){
+  switch(num3){
+    case 0:
+      leds[19] = CRGB::Black;
+    break;
+    case 1:
+       leds[16] = CRGB::Black;
+       leds[17] = CRGB::Black;
+       leds[19] = CRGB::Black;
+       leds[20] = CRGB::Black;
+       leds[21] = CRGB::Black;
+    break;
+    case 3:
+      leds[16] = CRGB::Black;
+      leds[20] = CRGB::Black;
+    break;
+    case 4:
+      leds[17] = CRGB::Black;
+      leds[20] = CRGB::Black;
+      leds[21] = CRGB::Black;
+    break;
+    case 5:
+      leds[18] = CRGB::Black;
+      leds[20] = CRGB::Black;
+    break;
+    case 6:
+      leds[18] = CRGB::Black;
+    break;
+    case 7:
+      leds[16] = CRGB::Black;
+      leds[19] = CRGB::Black;
+      leds[20] = CRGB::Black;
+      leds[21] = CRGB::Black;
+    break;
+    case 9:
+      leds[20] = CRGB::Black;
+    break;
+
+  }
+}
+
+void digit4(int num4){
+  switch(num4){
+    case 0:
+      leds[26] = CRGB::Black;
+    break;
+    case 1:
+       leds[23] = CRGB::Black;
+       leds[24] = CRGB::Black;
+       leds[26] = CRGB::Black;
+       leds[27] = CRGB::Black;
+       leds[28] = CRGB::Black;
+    break;
+    case 3:
+      leds[23] = CRGB::Black;
+      leds[27] = CRGB::Black;
+    break;
+    case 4:
+      leds[24] = CRGB::Black;
+      leds[27] = CRGB::Black;
+      leds[28] = CRGB::Black;
+    break;
+    case 5:
+      leds[25] = CRGB::Black;
+      leds[27] = CRGB::Black;
+    break;
+    case 6:
+      leds[25] = CRGB::Black;
+    break;
+    case 7:
+      leds[23] = CRGB::Black;
+      leds[26] = CRGB::Black;
+      leds[27] = CRGB::Black;
+      leds[28] = CRGB::Black;
+    break;
+    case 9:
+      leds[27] = CRGB::Black;
+    break;
+
+  }
+}
+
 void loop() {
   // put your main code here, to run repeatedly:
   fill_solid( leds, NUM_LEDS, CRGB::Red);
