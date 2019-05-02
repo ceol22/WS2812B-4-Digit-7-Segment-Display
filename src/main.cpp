84b6b20bcb2707ae7be1f501dbffbe9f05cd940c
ceol22 <jordywk2004@gmail.com>
2019-05-02 20:59:40 +0200

    Added Minutes

--- src/main.cpp
+++ src/main.cpp
@@ -224,7 +224,7 @@ void loop() {
   int hour = now.hour();
   int minute = now.minute();
 
-  fill_solid( leds, NUM_LEDS, CRGB::Red);
+  fill_solid( leds, NUM_LEDS, CRGB::Red);  // Set color
 
   switch(hour){
     case 0:
@@ -324,4 +324,250 @@ void loop() {
       digit2(3);
     break;
   }
+
+switch(minute){
+  case 0:
+    digit3(0);
+    digit4(0);
+  break;
+  case 1:
+    digit3(0);
+    digit4(1);
+  break;
+  case 2:
+    digit3(0);
+    digit4(2);
+  break;
+  case 3:
+    digit3(0);
+    digit4(3);
+  break;
+  case 4:
+    digit3(0);
+    digit4(4);
+  break;
+  case 5:
+    digit3(0);
+    digit4(5);
+  break;
+  case 6:
+    digit3(0);
+    digit4(6);
+  break;
+  case 7:
+    digit3(0);
+    digit4(7);
+  break;
+  case 8:
+    digit3(0);
+    digit4(8);
+  break;
+  case 9:
+    digit3(0);
+    digit4(9);
+  break;
+  case 10:
+    digit3(1);
+    digit4(0);
+  break;
+  case 11:
+    digit3(1);
+    digit4(1);
+  break;
+  case 12:
+    digit3(1);
+    digit4(2);
+  break;
+  case 13:
+    digit3(1);
+    digit4(4);
+  break;
+  case 14:
+    digit3(1);
+    digit4(4);
+  break;
+  case 15:
+    digit3(1);
+    digit4(5);
+  break;
+  case 16:
+    digit3(1);
+    digit4(6);
+  break;
+  case 17:
+    digit3(1);
+    digit4(7);
+  break;
+  case 18:
+    digit3(1);
+    digit4(8);
+  break;
+  case 19:
+    digit3(1);
+    digit4(9);
+  break;
+  case 20:
+    digit3(2);
+    digit4(0);
+  break;
+  case 21:
+    digit3(2);
+    digit4(1);
+  break;
+  case 22:
+    digit3(2);
+    digit4(2);
+  break;
+  case 23:
+    digit3(2);
+    digit4(3);
+  break;
+  case 24:
+    digit3(2);
+    digit4(4);
+  break;
+  case 25:
+    digit3(2);
+    digit4(5);
+  break;
+  case 26:
+    digit3(2);
+    digit4(6);
+  break;
+  case 27:
+    digit3(2);
+    digit4(7);
+  break;
+  case 28:
+    digit3(2);
+    digit4(8);
+  break;
+  case 29:
+    digit3(2);
+    digit4(9);
+  break;
+  case 30:
+    digit3(3);
+    digit4(0);
+  break;
+  case 31:
+    digit3(3);
+    digit4(1);
+  break;
+  case 32:
+    digit3(3);
+    digit4(2);
+  break;
+  case 33:
+    digit3(3);
+    digit4(3);
+  break;
+  case 34:
+    digit3(3);
+    digit4(4);
+  break;
+  case 35:
+    digit3(3);
+    digit4(5);
+  break;
+  case 36:
+    digit3(3);
+    digit4(6);
+  break;
+  case 37:
+    digit3(3);
+    digit4(7);
+  break;
+  case 38:
+    digit3(3);
+    digit4(8);
+  break;
+  case 39:
+    digit3(3);
+    digit4(9);
+  break;
+  case 40:
+    digit3(4);
+    digit4(0);
+  break;
+  case 41:
+    digit3(4);
+    digit4(1);
+  break;
+  case 42:
+    digit3(4);
+    digit4(2);
+  break;
+  case 43:
+    digit3(4);
+    digit4(3);
+  break;
+  case 44:
+    digit3(4);
+    digit4(4);
+  break;
+  case 45:
+    digit3(4);
+    digit4(5);
+  break;
+  case 46:
+    digit3(4);
+    digit4(6);
+  break;
+  case 47:
+    digit3(4);
+    digit4(7);
+  break;
+  case 48:
+    digit3(4);
+    digit4(8);
+  break;
+  case 49:
+    digit3(4);
+    digit4(9);
+  break;
+  case 50:
+    digit3(5);
+    digit4(0);
+  break;
+  case 51:
+    digit3(5);
+    digit4(1);
+  break;
+  case 52:
+    digit3(5);
+    digit4(2);
+  break;
+  case 53:
+    digit3(5);
+    digit4(3);
+  break;
+  case 54:
+    digit3(5);
+    digit4(4);
+  break;
+  case 55:
+    digit3(5);
+    digit4(5);
+  break;
+  case 56:
+    digit3(5);
+    digit4(6);
+  break;
+  case 57:
+    digit3(5);
+    digit4(7);
+  break;
+  case 58:
+    digit3(5);
+    digit4(8);
+  break;
+  case 59:
+    digit3(5);
+    digit4(9);
+  break;
+}
+
+
+  FastLED.show();
 }
